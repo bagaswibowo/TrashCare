32371242b6ae6093e7f73dfc8154311810826d07
bagaswibowo <bagaswibowo121@gmail.com>
2019-04-24 22:07:45 +0700

    trashnews add fragment beranda

--- app/src/main/java/com/example/alhakim/trashcare/BerandaFragment.java
+++ app/src/main/java/com/example/alhakim/trashcare/BerandaFragment.java
@@ -1,17 +1,110 @@
 package com.example.alhakim.trashcare;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.squareup.picasso.Picasso;
 
 public class BerandaFragment extends Fragment {
+    private RecyclerView mPeopleRV;
+    private DatabaseReference mDatabase;
+    private FirebaseRecyclerAdapter<Item, BerandaFragment.ItemViewHolder> mPeopleRVAdapter;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_beranda, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Item");
+        mDatabase.keepSynced(true);
+
+        mPeopleRV = (RecyclerView) view.findViewById(R.id.myRecycleView);
+
+        DatabaseReference personsRef = FirebaseDatabase.getInstance().getReference().child("Item");
+        Query personsQuery = personsRef.orderByKey();
+
+        mPeopleRV.hasFixedSize();
+        mPeopleRV.setLayoutManager(new LinearLayoutManager(this.getContext()));
+
+        FirebaseRecyclerOptions personsOptions = new FirebaseRecyclerOptions.Builder<Item>().setQuery(personsQuery, Item.class).build();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this.getContext(), 1);
+        mPeopleRV.setLayoutManager(gridLayoutManager);
+        mPeopleRVAdapter = new FirebaseRecyclerAdapter<Item, BerandaFragment.ItemViewHolder>(personsOptions) {
+            @Override
+            protected void onBindViewHolder(BerandaFragment.ItemViewHolder holder, final int position, final Item model) {
+                holder.setTitle(model.getTitle());
+                holder.setImage(getContext(), model.getImage());
+                holder.mView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        final String url = model.getUrl();
+                        Intent intent = new Intent(getContext(), ItemWebView.class);
+                        intent.putExtra("id", url);
+                        startActivity(intent);
+                    }
+                });
+            }
+
+            @Override
+            public BerandaFragment.ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+                View view = LayoutInflater.from(parent.getContext())
+                        .inflate(R.layout.item_row, parent, false);
+
+                return new BerandaFragment.ItemViewHolder(view);
+            }
+        };
+
+        mPeopleRV.setAdapter(mPeopleRVAdapter);
+
+
+    }
+    public static class ItemViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        public ItemViewHolder(View itemView){
+            super(itemView);
+            mView = itemView;
+        }
+        public void setTitle(String title){
+            TextView post_title = (TextView)mView.findViewById(R.id.post_title);
+            post_title.setText(title);
+        }
+        public void setImage(Context ctx, String image){
+            ImageView post_image = (ImageView) mView.findViewById(R.id.post_image);
+            Picasso.with(ctx).load(image).into(post_image);
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mPeopleRVAdapter.startListening();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mPeopleRVAdapter.stopListening();
     }
 }
